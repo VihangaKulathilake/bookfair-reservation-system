5f6716fc2d4ff76cb456538e4ca7c0f55ea72be0
Asal himsanda <150653318+asalhanadapangoda@users.noreply.github.com>
2026-02-17 14:54:58 +0530

    Resolve merge conflict and add user messages

--- backend/src/main/java/com/bookfair/backend/util/CommonMessages.java
+++ backend/src/main/java/com/bookfair/backend/util/CommonMessages.java
@@ -1,6 +1,13 @@
 package com.bookfair.backend.util;
 
 public class CommonMessages {
+<<<<<<< Updated upstream
+=======
+
+    // User
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String EMAIL_REQUIRED = "Email is required";
+>>>>>>> Stashed changes
     public static final String EMAIL_ALREADY_EXISTS = "Email already exists";
     public static final String INVALID_EMAIL_OR_PASSWORD = "Invalid email or password";
 }
