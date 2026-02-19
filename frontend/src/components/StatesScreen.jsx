8efa8e70016d146f7284fa45f6c1b7efa17697c1
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-02-19 11:06:07 +0530

    update statesScreen

--- frontend/src/components/StatesScreen.jsx
+++ frontend/src/components/StatesScreen.jsx
@@ -8,6 +8,7 @@ const StatusScreen = ({ type = 'success', title, message, actionLabel, onAction 
     const isSuccess = type === 'success';
 
     return (
+
         <Box
             sx={{
                 display: 'flex',
@@ -60,6 +61,7 @@ const StatusScreen = ({ type = 'success', title, message, actionLabel, onAction 
                 </Paper>
             </Grow>
         </Box>
+        
     );
 };
 
